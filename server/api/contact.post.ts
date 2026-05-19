3dd2d6c23efd224edfa6025c455287d1480ce784
César <cesar@pieta.tech>
2026-05-19 16:40:41 -0400

    fix: debug

--- server/api/contact.post.ts
+++ server/api/contact.post.ts
@@ -3,7 +3,8 @@ import { createClient } from '@supabase/supabase-js'
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig()
   const body = await readBody(event)
-  console.log(body)
+  console.log('API POST /contact', body)
+  console.log('API COnfig', config)
   const supabase = createClient(config.private.supabaseUrl, config.private.supabaseKey)
   const { data, error } = await supabase
     .schema(config.private.supabaseSchema)
@@ -15,7 +16,8 @@ export default defineEventHandler(async (event) => {
       budget: body.budget,
       message: body.message,
     })
-  console.log(data)
+  console.log('API POST /contact Data', data)
+  console.log('API POST /contact Error', error)
 
   if (error) {
     return { success: false, error: error.message, details: error.details }
